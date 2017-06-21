00a62630753224f2ad5d48eb871e5ad10f51487a
Yura <yurastich>
2017-06-21 14:31:26 +0300

    without mobile

--- dev/js/global.js
+++ dev/js/global.js
@@ -154,79 +154,6 @@ $(document).ready(function () {
 
 // popup end
 
-  var firstScreen = function () {
-    var header = $(".b-header").outerHeight(),
-      heightScreen = $(window).height(),
-      heightFirstFooter = $(".b-first-footer").outerHeight(),
-      cont = heightScreen - header - heightFirstFooter;
-    if (heightScreen >= 600) {
-      $(".e-first-container").css("height", cont);
-    } else {
-      $(".e-first-container").css("height", 431);
-    }
-
-  };
-
-  var firstSlider = new Swiper('.b-first-slider-swiper', {
-    nextButton: '.js-first-slider-next',
-    prevButton: '.js-first-slider-prev',
-    effect: 'fade',
-    keyboardControl: "true",
-    loop: true
-  });
-
-// todo Iphone Slider
-  // autoplay for iphone
-  var isPaused = false;
-  var t = window.setInterval(function() {
-    if(!isPaused) {
-      iphoneSlider.slideNext();
-    }
-  }, 3000);
-  var iphoneIs = $("div").is(".b-advantages");
-  if(!iphoneIs){
-    isPaused = true;
-  }
-  // autoplay for iphone
-
-
-  var changeScreenShot = function (index) {
-    if(index === 7){
-      index = 1;
-    }
-
-    var items = $(".b-advantages-list .e-advantages-item[data-index="+index+"]"),
-      textBlock = $(".b-advantages-text-block");
-    textBlock.removeClass("active");
-    items.find(".b-advantages-text-block").addClass("active");
-  };
-
-  var iphoneSlider = new Swiper('.e-iphone-slider-container.swiper-container', {
-    effect: 'fade',
-    loop: true,
-    speed: 500,
-    onTransitionStart: function(swiper){
-      changeScreenShot(swiper.activeIndex);
-    }
-  });
-
-
-  $(".b-advantages-list .e-advantages-item").click(function () {
-    var itemIndex = $(this).data("index");
-    changeScreenShot(itemIndex);
-    iphoneSlider.slideTo(itemIndex);
-  });
-
-  $(".e-advantages-flex-container").hover(function () {
-    iphoneSlider.stopAutoplay();
-    isPaused = true;
-  },function () {
-    iphoneSlider.startAutoplay();
-    isPaused = false;
-  });
-
-// todo END Iphone Slider
-
 // todo Select
   $(".js-click-sl, .b-select-container .e-select-top").click(function(){
     var par = $(this).closest(".b-select-container, .js-par-sl"),
@@ -265,117 +192,17 @@ $(document).ready(function () {
 
 // todo END Select
 
-// todo Sidebar
-  $(".b-arrow-sidebar").click(function () {
-    var openElem = $(this).closest(".b-sidebar").find("div"),
-        hasActive = $(this).hasClass("active"),
-        icons = $(".b-sidebar-menu .e-sidebar-item"),
-        lengthIcons = icons.length,
-        menu = $(".e-sidebar-menu-container");
-    if(!hasActive){
-      $(".b-sidebar-menu-easy-list").addClass("active");
-      $(this).addClass("active");
-      menu.slideDown();
-      // jump icons
-      var i = lengthIcons , ended = 0;
-      function iconsAnim() {
-        icons.eq(i).addClass("m-sidebar-item-jump m-sidebar-item-light m-sidebar-item-visible");
-        i--;
-        if( i >= ended ){
-          setTimeout( iconsAnim, 200)
-        }else{
-          setTimeout( function () {
-            icons.removeClass("m-sidebar-item-jump m-sidebar-item-light");
-          }, 2000)
-        }
-      }
-      // end jump icons
-      iconsAnim();
-    }else{
-      $(this).removeClass("active");
-      $(".b-sidebar-menu-easy-list").removeClass("active");
-      menu.slideUp();
-      icons.removeClass("m-sidebar-item-jump m-sidebar-item-light m-sidebar-item-visible");
-
-    }
-
+  var swiper = new Swiper('.e-first-container.swiper-container', {
+    pagination: '.e-first-container .swiper-pagination',
+    paginationClickable: true
   });
 
-  $(".e-sidebar-item").hover(function () {
-    $(this).css("z-index", 2);
-    $(this).next(".e-sidebar-item").css("z-index", 1);
-  }, function () {
-    $(this).next(".e-sidebar-item").css("z-index", 1);
+  var swiperGoods = new Swiper('.b-third-good-slider .swiper-container', {
+    slidesPerView: 4,
+    spaceBetween: 0,
+    nextButton: '.js-goods-next',
+    prevButton: '.js-goods-prev',
   });
-// todo END Sidebar
-
-// todo Right panel
-  $(".b-button-right-panel, .js-button-right-panel").click(function () {
-    var panel = $(".b-right-panel"),
-        active = panel.hasClass("active"),
-        buttons = $(".b-button-right-panel, .js-button-right-panel");
-
-    if(!active){
-      panel.addClass("active");
-      buttons.addClass("active");
-    }else{
-      panel.removeClass("active");
-      buttons.removeClass("active");
-    }
-  });
-// todo END Right panel
-
-// todo Open Content
-  var openContent = function (button) {
-    var button = $(button),
-        containerGlobal = button.closest(".js-openContentGlobal"),
-        container = containerGlobal.find(".js-openContentContainer"),
-        content = containerGlobal.find(".js-openContentContent"),
-        containerHeight = container.outerHeight(),
-        newHeight = content.outerHeight(),
-        savedHeight = container.attr("savedheight"),
-        active = button.hasClass("active");
-
-
-    if(!active){
-      container.attr("savedheight", containerHeight);
-
-      container.animate({
-        "height": newHeight
-      }, 300);
-      button.addClass("active");
-    }else{
-      container.animate({
-        "height": savedHeight
-      }, 300);
-      button.removeClass("active");
-    }
-  };
-
-  $(".js-openContentButton").click(function (e) {
-    e.preventDefault();
-    openContent(this);
-  });
-
-// todo END Open Content
-
-  var peopleCompany = new Swiper('.e-people-slider-container', {
-    slidesPerView: 6,
-    centeredSlides: false,
-    spaceBetween: 5
-  });
-
-  // todo Universal Trigger Active
-  $(".js-active").click(function () {
-    var active = $(this).hasClass("active");
-    if(!active){
-      $(this).addClass("active");
-    }else{
-      $(this).removeClass("active");
-    }
-  });
-  // todo END Universal Trigger Active
-
 
   androidAnimate();
   popup();
